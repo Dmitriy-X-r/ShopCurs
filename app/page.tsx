db38e1018dc261ca2da8b63c5a74456bb883cb41
Dmitriy <Batunin>
2025-08-05 01:22:36 +0700

    Добавил компонент 'кнопка'

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { Htag } from "./components";
+import { Button, Htag } from "./components";
 import styles from "./page.module.css";
 import React, { JSX } from "react";
 
 export default function Home(): JSX.Element {
   return (
     <div className={styles.page}>
-      <Htag tag="h3">Текст</Htag>
+      <Htag tag="h1">Текст</Htag>
+      <Button appearance="primary">Кнопка</Button>
+      <Button appearance="ghost">Button</Button>
     </div>
   );
 }
