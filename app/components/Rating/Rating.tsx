8a4754384f484f1977dccc0c64d13f0e809671ee
Dima <batunin.diman2003@gmail.com>
2025-08-22 17:43:04 +0700

    fff

--- app/components/Rating/Rating.tsx
+++ app/components/Rating/Rating.tsx
@@ -3,6 +3,7 @@ import { RatingProps } from "./Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
 import Image from "next/image";
+import StarIcon from "./StarIcon"
 
 export const Rating = ({ isEditable = false, rating, setRating, ...props }: RatingProps): JSX.Element => {
     const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
@@ -11,13 +12,14 @@ export const Rating = ({ isEditable = false, rating, setRating, ...props }: Rati
         const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
             return (
                 <>
-                    <Image
+                    {/* <Image
                         src="/icons/star.svg"
                         alt="звездочка"
                         height={20}
                         width={20}
                         priority
-                    />
+                    /> */}
+                    <StarIcon />
                 </>
             );
         });
