9b07d7df016ee103ae1dbfab277079e6569dc24f
Dmitriy <Batunin>
2025-08-24 20:32:15 +0700

    реализовал компонент рейтинга

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import { Button, Htag, P, Tag } from "./components";
+import { Button, Htag, P, Rating, Tag } from "./components";
 import styles from "./page.module.css";
 import React, { JSX, useState } from "react";
 
 export default function Home(): JSX.Element {
 
   const [counter, setCounter] = useState(0);
+  const [rating, setRating] = useState(3);
+
 
   return (
     <div className={styles.page}>
@@ -22,6 +24,8 @@ export default function Home(): JSX.Element {
       <Tag size="m" color="red" href="https://github.com/Dmitriy-X-r/ShopCurs">Не Обычный</Tag>
       <Tag size="s" color="green">Обычный</Tag>
       <Tag color="primary">Обычный</Tag>
+      <Rating isEditable={true} rating={rating} setRating={setRating} />
+      <Rating rating={5} />
     </div>
   );
 }
